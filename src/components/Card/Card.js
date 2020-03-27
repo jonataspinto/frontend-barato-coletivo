56880af38c91672d8ebd42f438be150443e64906
jonataspinto <jonatas.pferreira@b2wdigital.com>
2020-03-27 17:44:38 -0300

    fix: melhora logica para card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { node } from 'prop-types';
 import * as S from './CardStyled';
 
-const Card = ({ imgSrc, title, description, price }) => (
-  <S.CardContainer>
-    <S.Image src={imgSrc} />
-    <span style={{ padding: '10px' }}>
-      <p className="title" style={{ padding: '0 0 10px 0' }}>
-        {title}
-      </p>
-      <p>{description}</p>
-      <p>R${price}</p>
-    </span>
-    <button type="button">Button</button>
-  </S.CardContainer>
-);
+const Card = ({ children }) => <S.CardContainer>{children}</S.CardContainer>;
 
 Card.propTypes = {
-  imgSrc: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  price: PropTypes.string.isRequired,
+  children: node.isRequired,
 };
 
 export default Card;
--- src/components/Card/CardStyled.js
+++ src/components/Card/CardStyled.js
@@ -1,25 +1,8 @@
 import styled from 'styled-components';
 
 export const CardContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  height: 250px;
-  /* background-color: #c4c4c4; */
+  min-height: 250px;
+  padding: 8px;
   border: solid 1px ${props => props.theme.colors.primary};
   border-radius: 4px;
-  span {
-    .title {
-      max-width: 150px;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-    }
-  }
-`;
-
-export const Image = styled.img`
-  border-radius: 4px 4px 0 0;
-  width: 200px;
-  height: 40%;
 `;
