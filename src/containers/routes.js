a01be938c34d26f4acacc966c533cf45aa0722ae
jonataspinto <jonatas.pferreira@b2wdigital.com>
2020-03-27 17:35:01 -0300

    fix: melhora implemantacao para componente layout

--- src/containers/routes.js
+++ src/containers/routes.js
@@ -3,15 +3,18 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Payment from './Payment';
 import Offer from './Offer';
 import Home from './Home';
+import { Layout } from '../components';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/payment" component={Payment} />
-        <Route exact path="/offer/:id" component={Offer} />
+        <Layout>
+          <Route exact path="/" render={() => <Redirect to="/home" />} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/payment" component={Payment} />
+          <Route exact path="/offer/:id" component={Offer} />
+        </Layout>
       </Switch>
     </BrowserRouter>
   );
